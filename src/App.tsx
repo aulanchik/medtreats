8a380c4631a3a9db3108e18a0b0bafb88928c1f7
Artyom Ulanchik <artyom.u@outlook.com>
2024-08-05 03:54:51 +0100

    import header styles and use header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { Header } from "@/components";
 
-const App: React.FC = (): JSX.Element => <>Hello World</>;
+const App: React.FC = (): JSX.Element => (
+  <div className="App background">
+    <main className="main">
+      <Header />
+    </main>
+  </div>
+);
 
 export default App;
