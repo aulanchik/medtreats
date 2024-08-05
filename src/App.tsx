a49821ca28ea6e2e5c2a3438eaaa888dc4d397b6
Artyom Ulanchik <artyom.u@outlook.com>
2024-08-05 04:53:23 +0100

    add hero to display

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Header } from "@/components";
+import { Header, Hero } from "@/components";
 
 const App: React.FC = (): JSX.Element => (
   <div className="App background">
     <main className="main">
       <Header />
+      <Hero />
     </main>
   </div>
 );
