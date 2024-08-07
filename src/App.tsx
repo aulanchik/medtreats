6fd5953e8175fa48c06e45a348d4abfcec910190
Artyom Ulanchik <artyom.u@outlook.com>
2024-08-07 04:29:09 +0100

    display footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { Header, Hero } from "@/components";
+import { Header, Hero, Footer } from "@/components";
 
 const App: React.FC = (): JSX.Element => (
   <div className="App background">
     <main className="main">
       <Header />
       <Hero />
+      <Footer />
     </main>
   </div>
 );
