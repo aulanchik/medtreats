60640e074de81b220f3c15ad5a731c811c008e89
Artyom Ulanchik <45861755+aulanchik@users.noreply.github.com>
2024-08-07 04:32:45 +0100

    Footer component (#8)
    * dependency updates
    
    * import footer data
    
    * added footer component
    
    * added footer styles
    
    * display footer

---
+++ src/components/Footer/types.ts
@@ -0,0 +1,13 @@
+type FooterLink = {
+  key: number;
+  label: string;
+  href: string;
+};
+
+type SocialLink = {
+  key: number;
+  platform: string;
+  href: string;
+};
+
+export type { FooterLink, SocialLink };
