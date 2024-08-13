b305d0ea518c761f5cccad08364c1ebf001af9d7
Artyom Ulanchik <45861755+aulanchik@users.noreply.github.com>
2024-08-13 18:50:15 +0100

    Gallery section (#10)
    * added gallery data
    
    * enable gallery section
    
    * added to main section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { Header, Hero, Footer } from "@/components";
+import { Header, Hero, Footer, Gallery } from "@/components";
 
 const App: React.FC = (): JSX.Element => (
   <div className="App background">
     <main className="main">
       <Header />
       <Hero />
+      <Gallery />
       <Footer />
     </main>
   </div>
